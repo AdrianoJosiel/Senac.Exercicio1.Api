03769592b5d30ad2185e14fd8fd079e59159a4d0
AdrianoJosiel <adrianojosiel@hotmail.com>
2024-10-28 21:09:56 -0300

    Adicionar arquivos de projeto.

---
+++ Senac.Exercicio.Domain/Entities/PessoaEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace Senac.Exercicio.Domain.Entities
+{
+    public class PessoaEntity
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; private set; }
+        public bool Situacao {  get; set; } 
+
+        public PessoaEntity()
+        {
+        }
+
+        public PessoaEntity(int id, string nome, string cpf, DateTime dataNascimento, bool situacao)
+        {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+            DataNascimento = dataNascimento;
+            Situacao = situacao;
+        }
+       
+    }
+}
---
+++ Senac.Exercicio.Service/PessoaService.cs
@@ -0,0 +1,31 @@
+﻿using Senac.Exercicio.Domain.DTO;
+using Senac.Exercicio.Domain.Entities;
+using Senac.Exercicio.Infra.Repository;
+using System.Security;
+
+namespace Senac.Exercicio.Service
+{
+    public class PessoaService
+    {
+
+        public List<PessoaEntity> ObterPessoas()
+     => new PessoaRepository().ObterPessoas();
+
+        /*
+        public List<PessoaEntity> ObterPessoas()
+        {
+            List<PessoaEntity> objLista = new List<PessoaEntity>();
+            PessoaRepository repositorio = new PessoaRepository();
+            objLista = repositorio.ObterPessoas();
+            return objLista;
+        }*/
+
+        public bool GravarPessoa (PessoaInputModel p)        
+        {
+            
+            PessoaEntity obj = new PessoaEntity(0, p.Nome, p.Cpf, p.DataNascimento, true);
+            //(gambiarra de momento )
+            return new PessoaRepository().Gravar(obj);
+        }
+    }
+}
